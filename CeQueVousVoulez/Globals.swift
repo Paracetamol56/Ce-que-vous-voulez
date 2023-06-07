0eec0d0cdc286883172139c1716e79c77c524cb5
Mathéo Galuba <matheo.galu56@gmail.com>
2023-06-07 19:49:03 +0200

    Comment the API key

--- CeQueVousVoulez/Globals.swift
+++ CeQueVousVoulez/Globals.swift
@@ -7,4 +7,5 @@
 
 import Foundation
 
+// This is a fake key, please replace it with your own API key (https://developer.themoviedb.org/docs/getting-started)
 let apiKey: String = "9a8f7a5168ace33d2334ba1fe14a83fb"
